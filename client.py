a6a30bbd601b0bbde44391de8f07db7752f93bc3
Joy <gajjarjoy4@gmail.com>
2021-06-12 14:46:31 +0530

    Simple Chat App

---
+++ client.py
@@ -0,0 +1,20 @@
+import socket
+import sys
+import time
+
+s = socket.socket()
+host = input(str("Please enter the hostname of the server:"))
+port = 2020
+
+s.connect((host, port))
+print("Connected to chat server")
+
+while True:
+    incoming_message = s.recv(1024)
+    incoming_message = incoming_message.decode()
+    print("Server:", incoming_message)
+    message = input(str(">>"))
+    message = message.encode()
+    s.send(message)
+    print("Message has been sent")
+    print("")
